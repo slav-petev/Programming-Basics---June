de95b92c18a09f61155be90b85019d4eba06eaae
Slav Petkov <Slav Petkov@PC-1138>
2016-06-21 20:54:38 +0300

    Solved CatchTheButton probblem

---
+++ SImpleCalculations/16.CatchTheButton/CatchTheButtonPlayground.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace _16.CatchTheButton
+{
+    partial class CatchTheButtonPlayground
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCatchMe = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCatchMe
+            // 
+            this.btnCatchMe.Location = new System.Drawing.Point(279, 192);
+            this.btnCatchMe.Name = "btnCatchMe";
+            this.btnCatchMe.Size = new System.Drawing.Size(147, 34);
+            this.btnCatchMe.TabIndex = 0;
+            this.btnCatchMe.Text = "Catch Me";
+            this.btnCatchMe.UseVisualStyleBackColor = true;
+            this.btnCatchMe.Click += new System.EventHandler(this.btnCatchMe_Click);
+            this.btnCatchMe.MouseEnter += new System.EventHandler(this.btnCatchMe_MouseEnter);
+            // 
+            // CatchTheButtonPlayground
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1078, 522);
+            this.Controls.Add(this.btnCatchMe);
+            this.Name = "CatchTheButtonPlayground";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.CatchTheButtonPlayground_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCatchMe;
+    }
+}
+
---
+++ SImpleCalculations/16.CatchTheButton/CatchTheButtonPlayground.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _16.CatchTheButton
+{
+    public partial class CatchTheButtonPlayground : Form
+    {
+        private int _minButtonPositionX;
+        private int _maxButtonPositionX;
+        private int _minButtonPositionY;
+        private int _maxButtonPositionY;
+
+        public CatchTheButtonPlayground()
+        {
+            InitializeComponent();
+        }
+
+        private void CatchTheButtonPlayground_Load(object sender, EventArgs e)
+        {
+            _minButtonPositionX = Location.X;
+            _maxButtonPositionX = Location.X + Width;
+            _minButtonPositionY = Location.Y;
+            _maxButtonPositionY = Location.Y + Height;
+        }
+
+        private void btnCatchMe_MouseEnter(object sender, EventArgs e)
+        {
+            var newPositionRandomCoordinatesGenerator = new Random();
+
+            var newXCoordinate = newPositionRandomCoordinatesGenerator.Next(
+                _minButtonPositionX, _maxButtonPositionX);
+            var newYCoordinate = newPositionRandomCoordinatesGenerator.Next(
+                _minButtonPositionY, _maxButtonPositionY);
+
+            btnCatchMe.Location = new Point(newXCoordinate, newYCoordinate);
+        }
+
+        private void btnCatchMe_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("You win!");
+
+            btnCatchMe.MouseEnter -= btnCatchMe_MouseEnter;
+            btnCatchMe.Click -= btnCatchMe_Click;
+        }
+    }
+}
