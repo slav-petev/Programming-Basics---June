8535ff042469beef5ea8e5260c540e2c8c9b72d2
Slav Petkov <Slav Petkov@PC-1138>
2016-06-21 19:35:03 +0300

    Solved ThousandDaysAfterBirth problem

---
+++ SImpleCalculations/15.ThousandDaysAfterBirth/ThousandDaysAfterBirthDateCalculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+public class ThousandDaysAfterBirthDateCalculator
+{
+    public static void Main()
+    {
+        const string dateFormat = "dd-MM-yyyy";
+
+        var birthDate = DateTime.ParseExact(Console.ReadLine(), dateFormat,
+            CultureInfo.InvariantCulture);
+
+        var thousandDaysAfterBirthDate = birthDate.AddDays(1000);
+
+        var outputDateFormatted = thousandDaysAfterBirthDate.ToString(dateFormat);
+        Console.WriteLine(outputDateFormatted);
+    }
+}
